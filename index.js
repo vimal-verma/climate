05eb2cd01f997ce29524863da767dc0b001ac5b8
vimalverma558 <kumarvimal558@gmail.com>
2021-02-17 11:21:20 +0530

    vercel resetup 🚀

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('newrelic');
 const express = require('express');
+var path = require('path');
 
 const app = express()
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'public'));
 
 app.get('/',(req,res)=>{
     res.render('index')
